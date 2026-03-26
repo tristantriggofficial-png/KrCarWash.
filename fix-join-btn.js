fb8728ad0853cd5274c3dc7bba99cc3e9d95f14c
tristantriggofficial-png <tristantriggofficial@gmail.com>
2026-03-26 11:12:31 -0700

    Update fix-join-btn.js

--- fix-join-btn.js
+++ fix-join-btn.js
@@ -1,9 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var btns = document.querySelectorAll('.join-club-btn');
-  btns.forEach(function(btn) {
-    btn.addEventListener('click', function(e) {
-      e.preventDefault();
-      window.location.href = '/kr-membership-signup';
-    });
+  document.getElementById('membershipBtn').addEventListener('click', function() {
+    window.location.href = '/kr-membership-signup';
   });
 });
